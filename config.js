33abdc0775e9e36dacb6f8d6fdcf8ff2f3a0b238
Tatsuhiro404 <pangjingshuo05@gmail.com>
2026-09-12 18:46:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SHUNJI_SUPABASE_CONFIG = {
-  url: 'YOUR_SUPABASE_URL',
-  anonKey: 'YOUR_SUPABASE_ANON_KEY'
+  url: 'https://goxzdmhrhxonjxpvodtu.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdveHpkbWhyaHhvbmp4cHZvZHR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxODkzMTUsImV4cCI6MjEwNDc2NTMxNX0.RI0gW9AQwxFoazC5ihEhBfqY9-fFOhU6lmMiWCL58l4'
 };
